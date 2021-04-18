a8abeeec4d5c0dd24abbf7ef198a4b4d4899998e
Rakesh Pathania <PathaniaR@hotmail.com>
2021-04-18 20:36:39 +0530

    End of section 6: UI Changes

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { User } from '../_models/user';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -12,19 +12,25 @@ export class NavComponent implements OnInit {
   model:any={};
   loggedIn:boolean=false;
 
-  constructor(public accountService:AccountService) {
+  constructor(public accountService:AccountService,
+    private router:Router,
+    private toastrService:ToastrService) {
    }
 
   ngOnInit(): void {
+    console.log('Nav Component:Init')
   }
   login(){
     this.accountService.login(this.model).subscribe(response=>{
+      this.router.navigateByUrl('/members');
     },error=>{
       console.log("Error is",error);
+      this.toastrService.error(error.error);
     });
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
 @Output() cancelRegister:EventEmitter<boolean>=new EventEmitter<boolean>();
  model:any={};   
-  constructor(private accountService:AccountService) { }
+  constructor(private accountService:AccountService,private toastrService:ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +20,8 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(response=>{
        console.log('Response regiser',response);
        this.cancel();
+    },(error:any)=>{
+      this.toastrService.error(error.error);
     });
   }
 
